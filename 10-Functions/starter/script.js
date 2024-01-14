4ccd67a1b5baf5fad0afedea27f9ca147e72b4fa
Wicher <wicherICT@hotmail.com>
2024-01-14 18:42:13 +0100

    131 first class and higher order functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,36 +1,52 @@
 'use strict';
 
+// // 131 First class and higher order functions
+
+//FIRST CLASS FUNCTIONS
+// are first class citizens (they are treated as values and this is very special for Javascript). Function are just another type of object and objects are values, so funciton are simply values.
+// this means we can store functions as in variables or Object properties.
+// we can also pass function as arguments to other functions.
+//we can even return functions from other functions
+// remember functions are objects. so functions can have methods. which is craaaazy.
+
+//HIGHER ORDER FUNCTIONS
+//are functions that either 1: receives a function as an argument, 2:returns a new function or 3: Both.
+// this is only possible thanks to first class functions.
+
+//first class functions is just a feature that a languge has or does not hav. All it means is that all function are values. There are no first-class functions in practice. Its just a concept
+//there are however higher order functions in practice. Which are possible because the language reports first-class functions.
+
 // //------------------------------------------------------
 // // 130 Passing arguments value vs reference
 
-const flight = 'LH234';
-const Harry = {
-  name: 'Harry Barry',
-  passport: '12987457',
-};
+// const flight = 'LH234';
+// const Harry = {
+//   name: 'Harry Barry',
+//   passport: '12987457',
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === '12987457') {
-    alert('Check in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
+//   if (passenger.passport === '12987457') {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
 
-checkIn(flight, Harry);
-// for some reason flight isnt refactored like i the method. Because it is a primitive value. And primitives, when called, create a copy rather than being referenced
-console.log(flight);
-// but Harry howevef is refactored like in the method. because it refers to the same complex object. This can create unforeseen consequences.
-console.log(Harry);
+// checkIn(flight, Harry);
+// // for some reason flight isnt refactored like i the method. Because it is a primitive value. And primitives, when called, create a copy rather than being referenced
+// console.log(flight);
+// // but Harry howevef is refactored like in the method. because it refers to the same complex object. This can create unforeseen consequences.
+// console.log(Harry);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random) * 10000000000;
-};
-newPassport(Harry);
-checkIn(flight, Harry);
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random) * 10000000000;
+// };
+// newPassport(Harry);
+// checkIn(flight, Harry);
 
 // // // ------------------------------------------------------
 // // // 129 Default Parameters
