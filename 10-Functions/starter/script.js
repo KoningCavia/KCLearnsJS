c8c597c9cbf790acc303092f8997876d6eeb0122
Wicher <wicherICT@hotmail.com>
2024-01-14 18:04:26 +0100

    129 Default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+------------------------------------------------------
+// // 130 Passing arguments value vs reference
+
+
+------------------------------------------------------
+// // 129 Default Parameters
+// const bookings = [];
+
+// const createBooking = function (
+//   flighNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+
+//   const booking = {
+//     flighNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// //if we dont specify the third parameter, it calculated thanks to the constructor multiplication. Parameters need to be in proper order though.
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// //skippig the second parameter with undefined.
+// createBooking('LH123', undefined, 1000);
