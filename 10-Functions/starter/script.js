fc4d13883cf672c3ff755bbc5dfbd09da293d08a
Wicher <wicherICT@hotmail.com>
2024-01-14 19:09:53 +0100

    132 Functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,40 @@
 'use strict';
+// // 132  Functions accepting callback functions
 
-// // 131 First class and higher order functions
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`); // name is a baked in property of methods
+};
+
+// Here we are passing in a string and a function. See how we are not 'calling' the function (it doesnt end with '()'). we are merely passing it in like a value.
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+//first order function
+const high5 = function () {
+  console.log('👋');
+};
+
+//higher order function. (that returns the above first order function.)
+//JS uses callbacks ALL the time
+document.body.addEventListener('click', high5);
+//returns 3 waves. One for each name.
+['Harry', 'Pieternella', 'Pomni'].forEach(high5);
+
+//callback functions allow us to  reuse usefull code but more important
+// they allow us to create abstraction
+// // 131 First class and higher order functions. Like the transformer. we could have given it the code directly, but we have abstracted that away. the transformer doesnt care about what it does. (safety i guess?)
 
 //FIRST CLASS FUNCTIONS
 // are first class citizens (they are treated as values and this is very special for Javascript). Function are just another type of object and objects are values, so funciton are simply values.
