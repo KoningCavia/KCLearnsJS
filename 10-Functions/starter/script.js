222dc3cd73f4a893d67270c7cd3496f3ef7cd5d0
Wicher <wicherICT@hotmail.com>
2024-01-14 18:27:11 +0100

    130 Passing arguments  Value vs Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,11 +1,39 @@
 'use strict';
 
-------------------------------------------------------
+// //------------------------------------------------------
 // // 130 Passing arguments value vs reference
 
+const flight = 'LH234';
+const Harry = {
+  name: 'Harry Barry',
+  passport: '12987457',
+};
 
-------------------------------------------------------
-// // 129 Default Parameters
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === '12987457') {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, Harry);
+// for some reason flight isnt refactored like i the method. Because it is a primitive value. And primitives, when called, create a copy rather than being referenced
+console.log(flight);
+// but Harry howevef is refactored like in the method. because it refers to the same complex object. This can create unforeseen consequences.
+console.log(Harry);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random) * 10000000000;
+};
+newPassport(Harry);
+checkIn(flight, Harry);
+
+// // // ------------------------------------------------------
+// // // 129 Default Parameters
 // const bookings = [];
 
 // const createBooking = function (
